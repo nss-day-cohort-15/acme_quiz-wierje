a9851fe77bee0ab39f37b52efd7a9287c09c79f9
wierje <wier.je@gmail.com>
2016-08-23 11:47:34 -0500

    weekend work unspecified

--- scripts.js
+++ scripts.js
@@ -1,11 +1,43 @@
-$(document).ready(function () {
-  console.log('page ready');
-});
-(function(){
-  // my special code
+$(function() {
+    console.log('page ready');
+    // iife form and page load message
 
+    $('.navbar-header').html('<h1>Acme Explosives!</h1>');
+    $('.navbar-header').append('<img id="tnt" src="/img/dynamite.jpg">').hide().show(300);
+    $('.navbar-header').append('<img id="fireworks" src="/img/Scan-130817-0010.jpg" style="float: right"> ').hide().show(300);
 
+    $.ajax({
+        url: "categories.json"
+    }).done(function(fillOption) {
+        console.log("the contents of categories.json");
+        console.log(fillOption);
+        var categories = fillOption.categories;
+        var results = "";
+        for (var cat in categories) {
+          console.log("cat is", categories[cat].name);
+            var newitem = `<option> ${categories[cat].name}</option>`;
+            results += newitem;
 
+        };
+        $("#parent").empty();
+        $("#parent").html(results);
+        console.log("results", results);
 
-
+    });
 }());
+
+
+
+
+
+
+
+
+
+        // console.log(fillOption);
+
+        //     $("#parent").empty();
+        //     $.each(fillOption, function () {
+        //         $("#parent").append($("<option></option>").val(this['id']).html(this['name']));
+        //     });
+        // });
